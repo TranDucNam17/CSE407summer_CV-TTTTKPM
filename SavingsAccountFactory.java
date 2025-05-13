66912fb7668a1eb83cd743320fcccc5d7498fed5
Trần Đức Nam - NannXyls <167669035+TranDucNam17@users.noreply.github.com>
2025-05-13 22:43:30 +0700

    Factory Method - Class Diagram

---
+++ SavingsAccountFactory.java
@@ -0,0 +1,6 @@
+public class SavingsAccountFactory implements AccountFactory {
+    @Override
+    public Account createAccount(double initialBalance) {
+        return new SavingsAccount(initialBalance);
+    }
+}
